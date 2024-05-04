5cd6498f4abdf329fbce77a7a1013eceb985d4b2
Shivani Gupta <shivaniagrahari7819@gmail.com>
2024-05-04 11:29:09 +0530

    first commit

---
+++ TowerOfHanoi.java
@@ -0,0 +1,18 @@
+public class TowerOfHanoi {
+  public static void main(String[] args) {
+      int n=2;
+      solve(n,"S","H","D");
+  }
+    public static void solve(int n,String src,String helper,String dest)
+    {
+        if(n==1)
+        {
+            System.out.println("transfer disk" +n+"from "+ src+"to"+dest);
+            return;
+        }
+        solve(n-1,src,dest,helper);
+        System.out.println("transfer disk" +n+"from "+ src+"to"+dest);
+        solve(n-1,helper,src,dest);
+    }
+    
+}
